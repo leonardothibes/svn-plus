72677c5e7cf2cd9cf23190b0e0237b0dca06d8d8
Leonardo Thibes <leonardothibes@gmail.com>
2016-02-19 16:35:35 -0200

    Implementando...

--- svn-plus.sh
+++ svn-plus.sh
@@ -1,10 +1,71 @@
 #!/bin/bash
 
 VERSION=0.1.0
+COMMAND=help
+SVN=/usr/bin/svn
 
-BOLD=\033[1m
-ENDBOLD=\033[0m
+if [ ! -f ${SVN} ]; then
+	echo "Comando \"svn\" não encontrado!"
+	exit 1
+fi
 
+if [ ! -z $1 ]; then
+	COMMAND=$1
+fi
+
+##
+# Controla o fluxo do app.
+# 
+# @param $1 Ação a ser executada[tag|branch]
+# @param $2 Nome da tag/branch
+# @param $3 Flag de deleçao
+## 
+controller()
+{
+	ACTION=$1
+	NAME=$2
+	REMOVE=$3
+
+	if [[ ${ACTION} != 'tag' && ${ACTION} != 'branch' ]]; then
+		echo "Ação inválida: ${ACTION}"
+		exit 1
+	fi
+
+	if [ -z ${NAME} ]; then
+		list ${ACTION}
+		exit 0
+	fi
+}
+
+##
+# Extrai a URL do repositório do retorno do svn-info
+# 
+# @param $1 Define se é tag ou branch
+##
+url()
+{
+	svn info
+
+	# if [ $1 == 'tag']; then
+
+	# echo "http://svn.local"
+} 
+
+##
+# Lista tags ou branches
+# 
+# @param $1 Define se é tag ou branch
+## 
+list()
+{
+	URL=$(url $1)
+
+	echo "svn ls ${URL}"
+}
+
+##
+# Exibe a mensagem de HELP.
+##
 help()
 {
 	echo "Ferramenta de gerenciamento de TAGS e BRANCHES do Subversion (${VERSION})"
@@ -25,4 +86,4 @@ help()
 	echo "Para informações adicionais, veja https://bitbucket.org/lidercap/svn-plus/"
 }
 
-help
+eval controller ${COMMAND} $2 $3
